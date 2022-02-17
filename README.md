5bfccf4b4952fa5f2955979441d056922f4adc03
GH10PAVITHRA <pavithraadvikh17@gmail.com>
2022-02-17 10:40:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-gqc4ns
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-gqc4ns)
---
+++ src/app/userfile.ts
@@ -0,0 +1,40 @@
+export const UserFromFile = [
+  {
+    "userId": "U001",
+    "userName": "PAVI",
+    "password": "123@",
+    "contactNo": 8220640532,
+    "address": "1/34 NORTH STREET CHINNASALEM"
+},
+{
+    "userId": "U002",
+    "userName": "VINO",
+    "password": "456&",
+    "contactNo": 9443319432,
+    "address": "1/106 SIVAN KOVIL STREET ATTUR"
+},
+{
+    "userId": "U003",
+    "userName": "ADVIKH",
+    "password": "789*",
+    "contactNo": 9443089027,
+    "address": "2A JANAKI NAGAR SALEM"
+},
+{
+    "userId": "U004",
+    "userName": "SINDHU",
+    "password": "987$",
+    "contactNo": 9159911901,
+    "address": "1/150 KRISNASWAMY STREET CHENNAI"
+}
+
+
+
+
+
+
+
+
+
+
+];
